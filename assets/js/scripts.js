fe4c3f9f0fbbb30147d10dabb88f68354d45d1a9
Manuel Ruiz U <mruiz.u@icloud.com>
2024-03-11 10:04:27 -0300

    Prueba Finalizada

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,22 +1,21 @@
 
-$(document).ready(function(){
+$(document).ready(function () {
     $('.carousel').carousel();
 
     // Agrega un evento al formulario cuando se envíe
-    $('form').submit(function(e) {
-      // Evita que el formulario se envíe normalmente
-      e.preventDefault();
-      
-      // Muestra el mensaje de enviado correctamente
-      $('#mensaje-enviado').fadeIn();
-  
-      // Después de un tiempo, oculta el mensaje
-      setTimeout(function(){
-        $('#mensaje-enviado').fadeOut();
-      }, 3000); // 3000 milisegundos = 3 segundos
+    $('form').submit(function (e) {
+        // Evita que el formulario se envíe normalmente
+        e.preventDefault();
+
+        // Muestra el mensaje de enviado correctamente
+        $('#mensaje-enviado').fadeIn();
+
+        // Después de un tiempo, oculta el mensaje
+        setTimeout(function () {
+            $('#mensaje-enviado').fadeOut();
+        }, 3000); 
     });
 
-     // Inicializar tooltips
-     $('[data-toggle="tooltip"]').tooltip();
-  });
-  
+    // Inicializar tooltips
+    $('[data-toggle="tooltip"]').tooltip();
+});
